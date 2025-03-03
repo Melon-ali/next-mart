89368dd45d10aed579bca34bbd5056e872ab8ec2
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-03-03 15:32:55 +0600

    Add Reusable Data Table to Display Categories added

--- src/app/(WithDashboardLayout)/user/shop/category/page.tsx
+++ src/app/(WithDashboardLayout)/user/shop/category/page.tsx
@@ -1,12 +1,14 @@
-import ManageCategories from "@/components/modules/shop/category";
+import ManageCategories from '@/components/modules/shop/category'
+import { getAllCategories } from '@/services/Category'
 
+const ProductCategoryPage = async () => {
+  const {data, meta} = await getAllCategories()
+  console.log(data)
+  return (
+    <div>
+      <ManageCategories categories={data} />
+    </div>
+  )
+}
 
-const ProductCategoryPage = () => {
-    return (
-        <div>
-            <ManageCategories />
-        </div>
-    );
-};
-
-export default ProductCategoryPage;
+export default ProductCategoryPage
