8997ab7ed0d94c40ab37b7a62c74754321fb636f
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-03-03 10:16:39 +0600

    Modify Sidebar and Add Create Category Modal added

---
+++ src/app/(WithDashboardLayout)/user/shop/category/page.tsx
@@ -0,0 +1,12 @@
+import ManageCategories from "@/components/modules/shop/category";
+
+
+const ProductCategoryPage = () => {
+    return (
+        <div>
+            <ManageCategories />
+        </div>
+    );
+};
+
+export default ProductCategoryPage;
